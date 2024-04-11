3bdbf915797481c95c51b7819955b1e5155c624a
João Araújo <jpedro.araujo21@gmail.com>
2024-04-11 15:24:47 -0300

    First class: Position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+        Position position = new Position(1,3);
+        System.out.println(position);
     }
 }
