75a7f0232cf51105c5df70513cc81c24131daa81
João Araújo <jpedro.araujo21@gmail.com>
2024-04-13 21:03:45 -0300

    Implementing possible moves of King

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,8 +1,5 @@
 package boardgame;
 
-import chess.ChessPiece;
-import chess.ChessPosition;
-
 public class Board {
     private int rows;
     private int columns;
@@ -26,14 +23,14 @@ public class Board {
     }
 
     public Piece piece(int row, int column) {
-        if (!positionsExists(row, column)) {
+        if (!positionExists(row, column)) {
             throw new BoardException("Position not on the board");
         }
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
-        if (!positionsExists(position)) {
+        if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
         }
         return pieces[position.getRow()][position.getColumn()];
@@ -48,7 +45,7 @@ public class Board {
     }
 
     public Piece removePiece(Position position) {
-        if (!positionsExists(position)) {
+        if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
         }
         if (!thereIsAPiece(position)) {
@@ -60,16 +57,16 @@ public class Board {
         return aux;
     }
 
-    private boolean positionsExists(int row, int column) {
+    private boolean positionExists(int row, int column) {
         return row >= 0 && row < rows && column >= 0 && column < columns;
     }
 
-    public boolean positionsExists(Position position) {
-        return positionsExists(position.getRow(), position.getColumn());
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
     }
 
     public boolean thereIsAPiece(Position position) {
-        if (!positionsExists(position)) {
+        if (!positionExists(position)) {
             throw new BoardException("Position not on the board");
         }
         return piece(position) != null;
